768ebfc7ffbf09b12acbbea802940baf1c78b8de
Alexander Tolmachev <alexander.s.tolmachev@gmail.com>
2013-05-21 03:27:10 +0400

    Added project files

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+/*!
+ *\file main.cpp
+ *\brief Application entry point
+ */
+
+#include <iostream>
+#include <QtCore/QCoreApplication>
+
+int main(int argc, char *argv[]) {
+  QCoreApplication app(argc, argv);
+
+  return 0; 
+}
