541832ff227b6a523ee46afd2424fdfd2b6256bf
Alexander Tolmachev <alexander.s.tolmachev@gmail.com>
2013-05-21 14:33:40 +0400

    Added matrial definition

--- src/shape.h
+++ src/shape.h
@@ -7,21 +7,19 @@
 
 #include <QSharedPointer>
 
-#include "types.h"
-//#include "material.h"
+#include "material.h"
 
 class Shape;
-struct RayIntersection; 
 
 typedef QSharedPointer<Shape> ShapePointer;
 
 class Shape {
   public:
-//    Shape(MaterialPointer material) :mMaterial(material) {} 
-//    virtual ~Shape() {}
+    Shape(MaterialPointer material) :mMaterial(material) {} 
+    virtual ~Shape() {}
 
-//    MaterialPointer getMaterial() const { return mMaterial; }
+    MaterialPointer getMaterial() const { return mMaterial; }
     
   private:
-//    MaterialPointer mMaterial;
+    MaterialPointer mMaterial;
 };
